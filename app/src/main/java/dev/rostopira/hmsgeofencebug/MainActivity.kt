97920259ad72e5fd2e9779b5d6de5b34d151e7e8
Dima Rostopira <rostopiradv@gmail.com>
2020-08-14 16:30:48 +0300

    Increase reproduce speed and permission request for Android 10

--- app/src/main/java/dev/rostopira/hmsgeofencebug/MainActivity.kt
+++ app/src/main/java/dev/rostopira/hmsgeofencebug/MainActivity.kt
@@ -5,36 +5,44 @@ import android.app.Activity
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.WindowManager
 import android.widget.TextView
 import com.huawei.hms.location.Geofence
 import com.huawei.hms.location.GeofenceRequest
 import com.huawei.hms.location.LocationServices
-import kotlin.random.Random
 
 class MainActivity: Activity() {
     private var isReproduced = false
+    private var counter = 0
+    private var lastKnownLocation: Location? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
             requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 123)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
-            if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED)
-                requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 124)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
     }
 
+    @Suppress("UNUSED_PARAMETER")
     fun onClick(v: View?) {
-        v?.visibility = View.GONE
+        LocationServices.getFusedLocationProviderClient(this).lastLocation.addOnSuccessListener {
+            val wasNull = lastKnownLocation == null
+            lastKnownLocation = it
+            if (wasNull)
+                onClick(null)
+        }
+        val currentLocation = lastKnownLocation ?: return
         val geofence = Geofence.Builder()
             .setRoundArea(
-                Random.nextDouble(-90.0, 90.0),
-                Random.nextDouble(-90.0, 90.0),
-                Random.nextFloat() * 100
+                currentLocation.latitude,
+                currentLocation.longitude,
+                300f
             )
             .setConversions(Geofence.ENTER_GEOFENCE_CONVERSION)
             /** According to documentation:
@@ -58,6 +66,8 @@ class MainActivity: Activity() {
         locationServices.createGeofenceList(request, pendingIntent)
             .addOnSuccessListener {
                 /** Create again recursively to reproduce bug **/
+                counter++
+                Log.w("REGISTERED", counter.toString())
                 onClick(null)
             }
             .addOnFailureListener {
@@ -76,6 +86,17 @@ class MainActivity: Activity() {
                     onClick(null)
                 }
             }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 123 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            if (checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), 124)
     }
 
     companion object {
